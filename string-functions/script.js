56a17a6fb28e708f3e41cd5383d0ab0bb06a5c22
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-05-06 19:25:54 +0300

    string functions added

---
+++ string-functions/script.js
@@ -0,0 +1,9 @@
+let content = " Ece Sensoz - Ozgur Vurgun ";
+let content_lowerCase = content.toLocaleLowerCase(); //localler bolgenin alfabesine gore islem yapar
+let content_upperCase = content.toLocaleUpperCase();
+let content_lenght = content.length;
+let content_trim = content.trim();
+let content_slice = content.slice(0, 3);
+let content_split = content.split("-");
+let content_index = content[2];
+console.log(content_index);
