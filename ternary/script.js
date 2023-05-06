9760093e7948619a78903b8bd3e539c73f7d6fed
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-05-06 20:39:59 +0300

    ternary operator operations added

---
+++ ternary/script.js
@@ -0,0 +1,15 @@
+const userName = "ozgur";
+const password = "12345";
+
+/*
+
+if (password.length > 4) {
+  console.log("executing");
+}
+else{
+    console.log("not executing");
+} 
+
+*/
+
+password.length > 4 ? console.log("executing") : console.log("not executing");
