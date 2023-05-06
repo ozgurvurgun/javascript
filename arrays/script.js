f114bdada5120298a9c6b10962d3d14e81da2d1b
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-05-07 00:31:58 +0300

    arrays added

---
+++ arrays/script.js
@@ -0,0 +1,20 @@
+const PRODUCTS = [
+  "samsung",
+  "apple",
+  "huawei",
+  "xaiomi",
+  "sony",
+  "nokia",
+  "general mobile",
+];
+
+//! nested arrays
+
+const NEW_PRODUCT = ["asus", "acer", ["lenovo", "monster"]];
+// console.log(NEW_PRODUCT[2][1]);
+
+const NESTEDS_ARRAY = [];
+NESTEDS_ARRAY["apple"] = ["iphone", "macbook", "watch", "air"];
+NESTEDS_ARRAY["samsung"] = ["i20", "galaxy", "note"];
+
+console.log(NESTEDS_ARRAY);
